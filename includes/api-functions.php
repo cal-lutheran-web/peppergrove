9790addd0015f47c4d935724ca74eb584de0017a
Scott <stsmith@callutheran.edu>
2019-04-15 16:52:18 -0700

    added acf_ param option to filter posts on api request

--- includes/api-functions.php
+++ includes/api-functions.php
@@ -3,6 +3,7 @@
 // custom REST API parameters
 // append request parameter with acf_ to access a custom field
 
+
 function custom_request_param($args, $request){
 
 	foreach($_GET as $key=>$value){
@@ -20,9 +21,24 @@ function custom_request_param($args, $request){
 	return $args;
 }
 
-add_filter('rest_venues_query', 'custom_request_param', 99, 2);
+
+	
 
 
+function custom_request($request){
 
+	global $wp_query;
+	$post_types = get_post_types(array('_builtin'=>false,'public'=>true),'objects');
+
+	foreach($post_types as $pt){
+		
+		add_filter('rest_'.$pt->name.'_query', 'custom_request_param', 99,2);
+	}
+	add_filter('rest_post_query', 'custom_request_param', 99,2);
+
+
+}
+
+add_filter('rest_api_init', 'custom_request', 99, 2);
 
 ?>
