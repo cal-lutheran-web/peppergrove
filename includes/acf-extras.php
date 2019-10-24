68c40a78241fca18fc75d4d6ab34d97bba0fb658
Scott Smith <scottsmith24@gmail.com>
2019-10-24 14:40:10 -0700

    added faculty list ACF custom field

--- includes/acf-extras.php
+++ includes/acf-extras.php
@@ -3,7 +3,7 @@
 // add support for campus_location ACF custom field
 function acf_campus_locations_widget( $field ) {
 	
-	$location_data = json_decode(file_get_contents('https://earth.callutheran.edu/utilities/map/locations-json.php'));
+	$location_data = json_decode(file_get_contents('http://earth.callutheran.edu/utilities/map/locations-json.php'));
 	$location_array = array(
 		'' => ''
 	);
@@ -24,16 +24,16 @@ add_filter('acf/load_field/name=campus_location', 'acf_campus_locations_widget')
 // faculty search list
 function acf_faculty_list_widget( $field ) {
 	
-	$location_data = json_decode(file_get_contents('https://earth.callutheran.edu/utilities/map/locations-json.php'));
-	$location_array = array(
+	$faculty_data = json_decode(file_get_contents('http://earth.callutheran.edu/utilities/faculty/faculty-list-json.php'));
+	$faculty_array = array(
 		'' => ''
 	);
 
-	foreach($location_data as $slug => $name){		
-		$location_array[$name->slug] = $name->name;
+	foreach($faculty_data as $slug => $name){		
+		$faculty_array[$name->slug] = $name->name;
 	}
 	
-    $field['choices'] = $location_array;
+    $field['choices'] = $faculty_array;
 
     return $field;
     
