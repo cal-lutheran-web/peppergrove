aca13635c0c6edc7c4fac4d6f54d52cf3c9050fb
Scott Smith <stsmith@callutheran.edu>
2021-04-09 15:48:39 -0700

    added 404 error page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Page Not Found | Cal Lutheran</title>
+</head>
+<body>
+<div class="bg-box bg-gold-alt icon-alert">Page not found.</div>
+</body>
+</html>
